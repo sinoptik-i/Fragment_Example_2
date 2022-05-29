71329af4a8b61fcd094437b63c284ffdcfdd31c4
sinoptik_ <chestas@mail.ru>
2022-05-30 00:28:13 +0300

    Initial commit

---
+++ app/src/main/java/sin/android/fragmentexample2/FragmentOne.kt
@@ -0,0 +1,54 @@
+package sin.android.fragmentexample2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageButton
+import androidx.navigation.fragment.findNavController
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FragmentOne.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class FragmentOne : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_one, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val imgButton : ImageButton = view.findViewById(R.id.imgButton)
+        val editText : EditText = view.findViewById(R.id.editText)
+        val bundle = Bundle()
+
+        imgButton.setOnClickListener {
+            val name = editText.text
+            val hello = "Привет, $name"
+            bundle.putString("MyArg", hello)
+            findNavController().navigate(R.id.fragmentTwo, bundle)
+        }
+    }
+
+
+
+
+}
